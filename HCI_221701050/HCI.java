bdbec758e6c90beb01a16a5c1e6a46c6fe418acf
sathiya-sri <163719181+sathiya-sri@users.noreply.github.com>
2024-06-08 19:12:27 +0530

    Add files via upload

---
+++ HCI_221701050/HCI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hci;
+
+/**
+ *
+ * @author devar
+ */
+public class HCI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
